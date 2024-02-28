091869a1bb10593a3873417fc9f2c275e99243aa
SodiumBenjahmin420 <161543165+SodiumBenjahmin420@users.noreply.github.com>
2024-02-27 21:44:19 -0600

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,1 +1,23 @@
+local Players = game:GetService("Players")
 
+local GameList = {
+
+     ["6032399813"] = "Etrean Luminant";
+
+
+
+
+}
+
+
+function GameList:CheckGame()
+    
+    for i,Game in GameList do
+        
+        if Game == game.GameId then return game[1];
+        
+        end
+    end
+end
+
+return GameList
